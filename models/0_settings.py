a9b7b76f359272ed0ad36efaba063fa5151a55b3
Justin Lewis <jlew.blackout@gmail.com>
2011-05-10 14:18:52 -0400

    Moved some functions out of db.py into helpers and settings

---
+++ models/0_settings.py
@@ -0,0 +1,114 @@
+# coding: utf8
+DATE_FORMAT = "%m/%d/%y %I:%M %p"
+MAX_FILE_STORE = 20971520
+MAX_UPLOAD_SIZE = 10485760
+MAX_SIZE_ENG = "10mb"
+
+ALLOWED_HTML_TAGS = [
+    'a',
+    'b',
+    'blockquote',
+    'br/',
+    'em',
+    'i',
+    'li',
+    'ol',
+    'ul',
+    'u',
+    'p',
+    'cite',
+    'code',
+    'img/',
+    'table',
+    'tbody',
+    'tr',
+    'th',
+    'td',
+    'hr',
+    's',
+    'strike',
+    'sub',
+    'sup',
+    'div',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'span',
+    'font',
+    'strong',
+    #Flash Movies
+    'object',
+    'param',
+    'embed',
+    'meta',
+    'wbr',
+    'pre',
+ ] 
+
+ALLOWED_HTML_ATTR = {
+    'a': ['href', 'title', 'style'],
+    'img': ['src', 'alt', 'style','width','height'],
+    'blockquote': ['type', 'style'],
+    'p': ['style'],
+    'table': ['border', 'style'],
+    'span': ['style'],
+    'div': ['style'],
+    'font': ['style', 'face'],
+    #Flash Movies
+    'object':['width','height'],
+    'param':['name','value'],
+    'embed':['src','type','allowscriptaccess','allowfullscreen','width','height','wmode'],
+    'meta':['charset'],
+ }
+
+
+
+
+db = DAL('sqlite://storage.sqlite')
+    
+#########################################################################
+## Prepare Auth
+##
+## Get auth defined, tables will be added later on
+#########################################################################
+from gluon.tools import Auth
+from gluon.contrib.login_methods.ldap_auth import ldap_auth
+
+auth = Auth(globals(),db)                      # authentication/authorization
+
+auth.settings.create_user_groups = False
+auth.messages.label_remember_me = "Stay Logged In (for 30 days)"
+
+# RIT Ldap
+auth.settings.login_methods.append(ldap_auth(server='ldap.rit.edu', base_dn='ou=people,dc=rit,dc=edu'))
+
+## Prepare Email System
+from gluon.tools import Mail
+mail=Mail()
+mail.settings.server = 'smtp.gmail.com:587'
+mail.settings.tls = True
+mail.settings.sender = 'fossrit@gmail.com'
+mail.settings.login = 'fossrit:1CvTS3sIwT4hNz9Fh805TIDCc'
+
+auth.settings.mailer = mail
+auth.settings.registration_requires_verification = True
+auth.settings.registration_requires_approval = False
+
+auth.messages.verify_email = """Thanks for joining the innovation community at beta.innovation.rit.edu.
+
+Please click the following link to activate your new account.
+
+http://%s%s/%%(key)s
+
+Thanks again,
+
+The Innovation Team
+""" % ( request.env.http_host, URL(r=request,c='default',f='user',args=['verify_email']) )
+
+auth.settings.register_next = URL('user', args='login')
+    
+# DISABLE EXTRA FEATURES
+auth.settings.actions_disabled=['profile']
---
+++ models/db_helpers.py
@@ -0,0 +1,72 @@
+# coding: utf8
+def get_home_node():
+    if auth.user.home_node:
+        return db(db.node.id == auth.user.home_node).select().first()
+        
+def get_home_from_user(userid):
+    user = db(db.auth_user.id == userid).select().first()
+    return db(db.node.id == user.home_node).select().first()
+        
+def can_edit(node):
+    if auth.is_logged_in() and (node.type.public or node.id == auth.user.home_node):
+        return True
+    elif auth.has_membership("Site Admin"):
+        return True
+    return False
+
+def is_linked(node1, node2):
+    return db((db.linkTable.nodeId == node1) & (db.linkTable.linkId == node2)).count() or \
+           db((db.linkTable.nodeId == node2) & (db.linkTable.linkId == node1)).count()
+
+def get_node_links(current_node):
+    ## Grab nodes from Linked Table ##
+    ######TODO: THIS IS VERY UGLY, combine these into one statement
+    ######      so we don't need to do the rows lookup and dive directly
+    ######      into the formating statement
+    
+
+    # This loops through both sides of the link table
+    # adding each item to cat_dict which is a dictionary
+    # of categories that hold lists of nodes
+    cat_dict = {}
+    
+    #grab rows where nodeId == node.id
+    for row in db(db.linkTable.nodeId==current_node).select():
+    
+        # if the category has not been seen, add it to the dict with an empty list
+        if not cat_dict.has_key(row.linkId.type.value):
+            cat_dict[row.linkId.type.value] = []
+        
+        cat_dict[row.linkId.type.value].append(row.linkId)
+
+    #grab rows where linkId == node.id
+    for row in db(db.linkTable.linkId == current_node).select():
+    
+        # if the category has not been seen, add it to the dict with an empty list
+        if not cat_dict.has_key(row.nodeId.type.value):
+             cat_dict[row.nodeId.type.value] = []
+        cat_dict[row.nodeId.type.value].append(row.nodeId)
+        
+    return cat_dict
+
+def populate_node_with_required( node ):
+    for type in node.type.required_vocab:
+        db.nodeAttr.insert(nodeId=node.id, vocab=type, value="Change Me!")
+        
+
+### EDIT HELPERS
+def get_node_or_404( node_url ):
+    node = db(db.node.url == node_url).select().first()
+    
+    if not node:
+        raise HTTP(404, 'node not found')
+    else:
+        return node
+        
+def get_attribute_or_404( node, attribute_id ):
+    attr = db( (db.nodeAttr.id == attribute_id) & (db.nodeAttr.nodeId == node) ).select().first()
+    
+    if not attr:
+        raise HTTP(404, 'attribute not found')
+    else:
+        return attr
